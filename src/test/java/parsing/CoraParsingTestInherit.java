1cbe8a533908db216f3f55a0fc5133a01f3e9fb2
Bob Ruiken <bobruiken@gmail.com>
2019-10-08 19:21:49 +0200

    Added missing package definitions to the parsing tests

--- src/test/java/parsing/CoraParsingTestInherit.java
+++ src/test/java/parsing/CoraParsingTestInherit.java
@@ -13,6 +13,8 @@
  See the License for the specific language governing permissions and limitations under the License.
  *************************************************************************************************/
 
+package parsing;
+
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
--- src/test/java/parsing/TermReadingTest.java
+++ src/test/java/parsing/TermReadingTest.java
@@ -13,6 +13,8 @@
  See the License for the specific language governing permissions and limitations under the License.
  *************************************************************************************************/
 
+package parsing;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 import java.util.ArrayList;
